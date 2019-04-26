4345f1b3fab6ac4ec4e538aec08d91b86174f402
Damon C <gdamon@gmail.com>
2019-04-26 15:52:19 -0700

    simplified build, validate address with web3

--- src/components/registration.vue
+++ src/components/registration.vue
@@ -1,8 +1,8 @@
 <template>
     <b-jumbotron class="mechanics" lead="To receive tokens, register your public key here">
         <b-form @submit.prevent="onSubmit">
-        <b-form-input v-model="address" placeholder="Your public key"></b-form-input>
-        <b-btn class="mt-3" variant="primary" type="submit">ok</b-btn>
+        <b-form-input v-model="address" placeholder="Your address"></b-form-input>
+        <b-btn :disabled="!validAddress" class="mt-3" variant="primary" type="submit">ok</b-btn>
         </b-form>
         <b-alert class="mt-3" show v-if="error" variant="danger">{{error}}</b-alert>
         <b-alert class="mt-3" show v-if="success" variant="success">{{address}} successfully registered.</b-alert>
@@ -11,6 +11,8 @@
 
 <script>
 import http from '@/http';
+import { checkAddressChecksum } from 'web3-utils';
+
 export default {
   data() {
     return {
@@ -26,8 +28,20 @@ export default {
         this.success = true;
       }).catch((err) => {
         this.success = false;
-        this.error = err.response.data;
+        if (!err.reponse){
+            this.error = "No response from the server.  Try again later."
+        } else{
+          this.error = err.response.data;
+        }
       });
+    },
+  },
+  computed: {
+    validAddress() {
+      if (this.address.length >= 40) {
+        return checkAddressChecksum(this.address);
+      }
+      return false;
     },
   },
 }
